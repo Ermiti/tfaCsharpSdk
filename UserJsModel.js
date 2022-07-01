abedd250837798f88eccb9c4e0ef64f006e0b4f0
Ermiti <99094357+Ermiti@users.noreply.github.com>
2022-07-01 15:49:36 +0430

    Add files via upload

---
+++ UserJsModel.js
@@ -0,0 +1,19 @@
+// JavaScript source code
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userModule = new Schema({
+    uid: {
+        type: Number,
+        required: true
+    },
+    token: {
+        type: String,
+        required: true
+    },
+}, { timestamps: true });
+
+const User = mongoose.model('user', userModule);
+
+module.exports = User;
