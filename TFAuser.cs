cd43e787c429ed36ad891ff588fc558e50e7cded
Ermiti <99094357+Ermiti@users.noreply.github.com>
2022-07-03 13:12:19 +0430

    Update TFAuser.cs

--- TFAuser.cs
+++ TFAuser.cs
@@ -11,16 +11,12 @@ namespace tfaCSharpSdk
     class TFAuser
     {
 
-        private string JsonConverter(string v)
-        {
-            throw new NotImplementedException();
-        }
         public string user(string UserId, string TokenId)
          {
             string Server = "https://tele-fa-api.herokuapp.com/api/access/";
             string v=  $"{Server}{TokenId}/{UserId}";
           
-           return JsonConverter(v);
+             return JsonConvert.SerializeObject(v);
         }
 
       
